e72881a1ed2db5cf679f79ed631ef8e0184009fb
Logan S. Ahlstrom <lsahlstr@gmail.com>
2015-11-12 09:25:46 -0500

    read_distance_rmsd.R edited to read data structure M in vector form

--- bin/read_distance_rmsd.R
+++ bin/read_distance_rmsd.R
@@ -1,24 +1,28 @@
 #######################################################################
-# R code to read distance and RMSD information as part of a Genetic 
+# R script to read distance and RMSD information as part of a Genetic 
 # Algorithm-based force-field optimization for coarse-grained protein-
 # protein binding simulations. The distance and RMSD information for each
 # system considered in the optimization needs to be stored in its own 
-# subdirectory (e.g., "hdea_lj" for simulations of HdeA run using a Lennard
-# Jones potential for the inter-protein interactions).
+# subdirectory (e.g., with a name corresonding to the 4-letter PDB ID)
 #
-# Within these subdirectories, the distance information is the 1/r^12, 1/r^10, 
-# and 1/r^6 terms for solving the standar 12-6 Lennard Jones ("LJ") or 
-# 12-10-6 ("ETEN") potentials expressed in matrix form. They are named "n_r12ij.txt"
-# and "nn_r12ij.txt" for the 1/r^12 term for the native and non-native pools,
-# respectively. RMSD informatoin is stored as "n_rmsd.dat" and "nn_rmsd.dat" 
+# For each binding pose included in the optimization procedure, the 
+# inter-molecular pairwise distance information is stored in a single file with
+# the following format:
+#	#ResPair  # distance (r_ij)
+#	LYSPRO	  19.895
+#	LYSVAL	  19.917
+#	GLYLYS	  16.950
+#	LYSPHE	  19.580
+#	...
+# These files need to be named according to the following format:
+# 	[pool]_rij.[pose].txt
+#			pool => native ("n") or non-native ("nn")
+#			pose => an integeger to delineate poses within a given pool 
+#
+# RMSD information is stored as "n_rmsd.dat" and "nn_rmsd.dat" 
 # within each of the system subdirectories.
 #
-#       The initial version of the routine was written for the optimization
-#		of energy parameters with a Lennard Jones function 
-#               Aaron Frank, U. Michigan, c. 2015
-#       The subroutine was further developed to consider energies and distances
-#		simultaneously and with the 12-10-6 potential function
-#               Logan S. Ahlstrom, U. Michigan c. 2015
+# Logan S. Ahlstrom and Aaron T. Frank, U. Michigan c. 2015
 #######################################################################
 
 
@@ -36,22 +40,21 @@ readPool <- function(pool="n",lengthm) {
 		# read in data
 		R <- read.table(paste(pdb,"/",pool,"_rij.",conf,".txt",sep=""),col.names=c("pair","rij"))
 
-		# Get system info
-		#if (conf == 1) {		
-			info <- NULL
-			info[1] <- 0
-			if (pool == "n") {
-				info[1] <- 1
-			}
-			info[2] <- p
-			info[3] <- poolRMSD[conf]
-			info[4] <- conf
-		#}
+		# Get system info		
+		info <- NULL
+		info[1] <- 0
+		if (pool == "n") {
+			info[1] <- 1
+		}
+		info[2] <- p
+		info[3] <- poolRMSD[conf]
+		info[4] <- conf
 
 		# order based on pair (resnames of the ij pair)
 		R <- R[order(R$pair),]
-
 		R$pair <- as.character(R$pair)
+		
+		# Reference with all 210 aimino acid pairs
 		names <- as.character(respairs)
 
 		# Loop over ij keys and create list (and matrix)
